f6561b366481ff0581db7193ed133abdc107db9a
root <root@ip-172-31-36-96.us-east-2.compute.internal>
2024-10-17 11:54:11 +0000

    my third commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node this is my first node js pipeline!\n'
+  const msg1 = 'this is my second  build!\n'
+  res.end(msg1);
   res.end(msg);
 });
 
